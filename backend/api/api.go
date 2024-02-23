852f79564741a22c1fe33724611833bc802fc2c3
Utkarsh Chourasia <5.utkarshchourasia@gmail.com>
2024-02-23 09:19:47 +0530

    feat(core): Added package summary
    Package summary includes count of internal, external and testing packages.
    
    Signed-off-by: Utkarsh Chourasia <5.utkarshchourasia@gmail.com>

--- backend/api/api.go
+++ backend/api/api.go
@@ -3,20 +3,12 @@ package api
 import (
 	"encoding/json"
 	"fmt"
-	"log"
-	"main/core"
 	"net/http"
-	"net/url"
-	"os"
-	"os/exec"
-	"strings"
-
-	"github.com/JammUtkarsh/depth"
 )
 
 type Options struct {
 	Version bool   `json:"version"` // core
-	Path    string `json:"path"`    // extra
+	Path    string `json:"path"`    // core
 	StdLib  bool   `json:"stdlib"`  // semi-core
 	Branch  string `json:"branch"`  // extra
 }
@@ -56,52 +48,3 @@ func resolve(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func resolveRepo(repoURL string, opts Options) ([]byte, error) {
-	dirName := repoURL[strings.LastIndex(repoURL, "/")+1:]
-	os.RemoveAll(dirName)
-	if err := exec.Command("git", "clone", repoURL).Run(); err != nil {
-		return nil, err
-	}
-	var t depth.Tree = depth.Tree{
-		Root:         &depth.Pkg{},
-		OutputStdLib: opts.StdLib,
-		Version:      opts.Version,
-	}
-	log.Printf("Resolving for %s with %+v\n", dirName, opts)
-	os.Chdir(dirName)
-	if v, err := core.HandlePkgs(&t, opts.Path); err != nil {
-		os.RemoveAll(dirName)
-		return nil, err
-	} else {
-		os.Chdir("..")
-		os.RemoveAll(dirName)
-		return v, nil
-	}
-}
-
-func (p *Payload) checks() error {
-	p.RepoURL = strings.TrimSpace(p.RepoURL)
-	if p.RepoURL == "" {
-		return fmt.Errorf("repoURL is required")
-	}
-	if err := isValidURL(p.RepoURL); err != nil {
-		return fmt.Errorf("%v is an invalid url", p.RepoURL)
-	}
-	if p.Opts.Path != "" {
-		p.Opts.Path = strings.TrimSpace(p.Opts.Path)
-		p.Opts.Path = strings.Join([]string{".", p.Opts.Path}, "/")
-	} else {
-		p.Opts.Path = "."
-	}
-	return nil
-}
-
-func isValidURL(input string) error {
-	if !strings.HasPrefix(input, "http") {
-		return fmt.Errorf("invalid url")
-	}
-	if _, err := url.Parse(input); err != nil {
-		return err
-	}
-	return nil
-}
--- backend/core/core.go
+++ backend/core/core.go
@@ -8,20 +8,61 @@ import (
 	"github.com/JammUtkarsh/depth"
 )
 
+type Summary struct {
+	NumInternal int `json:"internal"`
+	NumExternal int `json:"external"`
+	NumTesting  int `json:"testing"`
+}
+
 func HandlePkgs(t *depth.Tree, pkg string) ([]byte, error) {
 	err := t.Resolve(pkg)
 	if err != nil {
 		fmt.Printf("'%v': FATAL: %v\n", pkg, err)
 		return nil, err
 	}
-	res := ToJSON(t)
-	return res, nil
+	res := struct {
+		Tree    *depth.Pkg `json:"tree"`
+		Summary Summary    `json:"summary"`
+	}{
+		Tree:    t.Root,
+		Summary: WritePkgSummary(*t.Root),
+	}
+	return ToJSON(res)
 }
 
-func ToJSON(t *depth.Tree) []byte {
+// writePkgSummary writes a summary of all packages in a tree
+func WritePkgSummary(pkg depth.Pkg) Summary {
+	var sum Summary
+	set := make(map[string]struct{})
+	for _, p := range pkg.Deps {
+		collectSummary(&sum, p, set)
+	}
+	return sum
+}
+
+func collectSummary(sum *Summary, pkg depth.Pkg, nameSet map[string]struct{}) {
+	if _, ok := nameSet[pkg.Name]; !ok {
+		nameSet[pkg.Name] = struct{}{}
+		if pkg.Internal {
+			sum.NumInternal++
+		} else {
+			sum.NumExternal++
+		}
+		if pkg.Test {
+			sum.NumTesting++
+		}
+		for _, p := range pkg.Deps {
+			collectSummary(sum, p, nameSet)
+		}
+	}
+}
+
+func ToJSON(v any) ([]byte, error) {
 	var buf bytes.Buffer
 	e := json.NewEncoder(&buf)
 	e.SetIndent("", "  ")
-	e.Encode(t.Root)
-	return buf.Bytes()
+	if err := e.Encode(v); err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
 }
