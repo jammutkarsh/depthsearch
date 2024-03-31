d5bf44804adf8b190144950cf5e8b730d7ad75fa
Tejaswini-Tiwari <tiwaritejaswini2601@gmail.com>
2024-03-31 10:54:45 +0530

    fix:react-icon

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,9 +1,9 @@
-import { LuNetwork } from "react-icons/lu";
+// import { LuNetwork } from "react-icons/lu";
 
 export default function Navbar() {
   return (
     <div>
-      <div className="text-4xl flex justify-center font-customFont"><LuNetwork />DepthSearch</div>
+      <div className="text-4xl flex justify-center font-customFont">DepthSearch</div>
       
       
     </div>
