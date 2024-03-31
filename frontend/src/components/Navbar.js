2324f720c4a8c620d63a0007b259dea98dc9aace
Utkarsh( उत्कर्ष ) <5.utkarshchourasia@gmail.com>
2024-03-31 11:31:59 +0530

    Revert "feat: SearchBar is updated" (#14)

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,11 +1,7 @@
-// import { LuNetwork } from "react-icons/lu";
-
 export default function Navbar() {
   return (
     <div>
       <div className="text-4xl flex justify-center font-customFont">DepthSearch</div>
-      
-      
     </div>
   )
 }
