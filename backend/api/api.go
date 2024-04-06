4c45b78b3cd04f721d4efad64159dbcf0f5b2590
Utkarsh( उत्कर्ष ) <5.utkarshchourasia@gmail.com>
2024-04-06 15:43:28 +0530

    Fix: Enable CORS (#15)
    Signed-off-by: Utkarsh Chourasia <5.utkarshchourasia@gmail.com>

--- backend/api/api.go
+++ backend/api/api.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/rs/cors"
 )
 
 type Options struct {
@@ -21,14 +23,20 @@ type Payload struct {
 func Server() error {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/resolve", resolve)
+	handler := cors.Default().Handler(mux)
+
 	fmt.Println("Server started at :8080")
-	if err := http.ListenAndServe(":8080", mux); err != nil {
+	if err := http.ListenAndServe(":8080", handler); err != nil {
 		return err
 	}
 	return nil
 }
 
 func resolve(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Only POST is allowed", http.StatusMethodNotAllowed)
+		return
+	}
 	var payload Payload
 	if err := json.NewDecoder(r.Body).Decode(&payload); err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
@@ -39,7 +47,7 @@ func resolve(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	if v, err := resolveRepo(payload.RepoURL, payload.Opts); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		http.Error(w, "unable to resolve the given path", http.StatusInternalServerError)
 		return
 	} else {
 		w.Header().Set("Content-Type", "application/json")
--- backend/go.mod
+++ backend/go.mod
@@ -2,4 +2,7 @@ module main
 
 go 1.20
 
-require github.com/JammUtkarsh/depth v0.0.0-20240208101935-105f6d4ba6ce
+require (
+	github.com/JammUtkarsh/depth v0.0.0-20240208101935-105f6d4ba6ce
+	github.com/rs/cors v1.10.1
+)
